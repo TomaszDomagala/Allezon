5ac8f5f443f3d89d4d3f84c6ef662b987d36f932
Tomasz <tomasz.domagala.contact@gmail.com>
2023-01-22 16:10:38 +0100

    rollback producer.go changes

--- src/pkg/messaging/producer.go
+++ src/pkg/messaging/producer.go
@@ -3,7 +3,6 @@ package messaging
 import (
 	"encoding/json"
 	"fmt"
-	"hash/adler32"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -17,35 +16,17 @@ type UserTagsProducer interface {
 }
 
 type Producer struct {
-	logger           *zap.Logger
-	client           sarama.Client
-	producer         sarama.SyncProducer
-	partitionsNumber int32
+	logger   *zap.Logger
+	producer sarama.SyncProducer
 }
 
 func NewProducer(logger *zap.Logger, addresses []string) (*Producer, error) {
-	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
-
-	client, err := sarama.NewClient(addresses, config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create kafka client: %w", err)
-	}
-	producer, err := sarama.NewSyncProducerFromClient(client)
+	producer, err := sarama.NewSyncProducer(addresses, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create producer: %w", err)
 	}
-	partitionsNumber, err := getPartitionNumber(client, UserTagsTopic)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get partitions number: %w", err)
-	}
 
-	return &Producer{
-		logger:           logger,
-		client:           client,
-		producer:         producer,
-		partitionsNumber: partitionsNumber,
-	}, nil
+	return &Producer{logger: logger, producer: producer}, nil
 }
 
 func (p *Producer) Send(tag types.UserTag) error {
@@ -58,7 +39,7 @@ func (p *Producer) Send(tag types.UserTag) error {
 	partition, offset, err := p.producer.SendMessage(&sarama.ProducerMessage{
 		Topic:     UserTagsTopic,
 		Value:     sarama.ByteEncoder(tagJson),
-		Partition: tagPartition(tag, p.partitionsNumber),
+		Partition: 0,
 	})
 
 	logOpts := []zap.Field{
@@ -72,18 +53,4 @@ func (p *Producer) Send(tag types.UserTag) error {
 	}
 	p.logger.Debug("kafka message sent", append(logOpts, zap.Int32("partition", partition), zap.Int64("offset", offset))...)
 	return nil
-}
-
-// getPartitionNumber returns the number of partitions for the given topic.
-func getPartitionNumber(client sarama.Client, topic string) (int32, error) {
-	partitions, err := client.Partitions(topic)
-	if err != nil {
-		return 0, fmt.Errorf("failed to get partitions: %w", err)
-	}
-	return int32(len(partitions)), nil
-}
-
-// tagPartition hashes tag.Cookie and returns the modulo of the number of partitions.
-func tagPartition(tag types.UserTag, partitionsNumber int32) int32 {
-	return int32(adler32.Checksum([]byte(tag.Cookie))) % partitionsNumber
 }
