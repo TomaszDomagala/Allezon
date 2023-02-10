26870b4504113ae0b66075292995910945dde83e
Hubert Badocha <badocha.hubert@gmail.com>
2023-02-10 17:32:44 +0100

    Moved insertion to user profiles to api (#39)
    * Moved insertion to user profiles to api
    
    * Changed to backoff

--- src/cmd/api/server/userTags.go
+++ src/cmd/api/server/userTags.go
@@ -1,13 +1,20 @@
 package server
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"time"
 
-	"go.uber.org/zap"
-
-	"github.com/TomaszDomagala/Allezon/src/pkg/dto"
-
+	"github.com/cenkalti/backoff/v4"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/TomaszDomagala/Allezon/src/pkg/db"
+	"github.com/TomaszDomagala/Allezon/src/pkg/dto"
+	"github.com/TomaszDomagala/Allezon/src/pkg/types"
 )
 
 func (s server) userTagsHandler(c *gin.Context) {
@@ -28,10 +35,63 @@ func (s server) userTagsHandler(c *gin.Context) {
 		_ = c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
-	if err := s.producer.Send(userTag); err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
+	var errGrp errgroup.Group
+
+	errGrp.Go(func() error {
+		b := backoff.NewExponentialBackOff()
+		b.MaxElapsedTime = 70 * time.Millisecond
+		b.InitialInterval = 10 * time.Millisecond
+		err := backoff.Retry(func() error {
+			return updateUserProfile(userTag, s.db.UserProfiles())
+		}, b)
+		if err != nil {
+			return fmt.Errorf("adding user profiles timeout, %w", err)
+		}
+		return nil
+	})
+	errGrp.Go(func() error {
+		return s.producer.Send(userTag)
+	})
+	if err := errGrp.Wait(); err != nil {
+		_ = c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-
 	c.Status(http.StatusNoContent)
+}
+
+const maxLen = 200
+
+func updateUserProfile(tag types.UserTag, userProfiles db.UserProfileClient) error {
+	up, err := userProfiles.Get(tag.Cookie)
+	if err != nil && !errors.Is(err, db.KeyNotFoundError) {
+		return fmt.Errorf("error getting tag, %w", err)
+	}
+	var arrPtr *[]types.UserTag
+	switch tag.Action {
+	case types.Buy:
+		arrPtr = &up.Result.Buys
+	case types.View:
+		arrPtr = &up.Result.Views
+	default:
+		return fmt.Errorf("unknown action, %d", tag.Action)
+	}
+	var newArr []types.UserTag
+	for i, t := range *arrPtr {
+		if t.Time.Before(tag.Time) {
+			newArr = slices.Insert(*arrPtr, i, tag)
+			break
+		}
+	}
+	if newArr == nil {
+		newArr = append(*arrPtr, tag)
+	}
+	if len(newArr) > maxLen {
+		newArr = newArr[:maxLen]
+	}
+	*arrPtr = newArr
+
+	if err := userProfiles.Update(tag.Cookie, up.Result, up.Generation); err != nil {
+		return fmt.Errorf("error updating tag, %w", err)
+	}
+	return nil
 }
--- src/cmd/worker/worker/worker.go
+++ src/cmd/worker/worker/worker.go
@@ -37,23 +37,13 @@ var numProcessors = runtime.NumCPU()
 
 func (w worker) Run(ctx context.Context) error {
 	tagsChan := make(chan types.UserTag, chanSize)
-	userTagsChan := make(chan types.UserTag, chanSize)
 	aggregatesChan := make(chan types.UserTag, chanSize)
 	defer close(tagsChan)
-	defer close(userTagsChan)
 	defer close(aggregatesChan)
 
 	for i := 0; i < numProcessors; i++ {
-		go runAggregatesProcessor(aggregatesChan, w.idGetter, w.db.Aggregates(), w.logger)
-		go runUpdateUserProfileProcessor(userTagsChan, w.db.UserProfiles(), w.logger)
+		go runAggregatesProcessor(tagsChan, w.idGetter, w.db.Aggregates(), w.logger)
 	}
-
-	go func() {
-		for tag := range tagsChan {
-			userTagsChan <- tag
-			aggregatesChan <- tag
-		}
-	}()
 
 	if err := w.consumer.Consume(ctx, tagsChan); err != nil {
 		return fmt.Errorf("error consuming messages, %w", err)
